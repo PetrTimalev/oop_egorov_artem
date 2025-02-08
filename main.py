f392eeef89c1f540f063f5b6c6823c32c2df7820
petrtimalev <petr.timalev@gmail.com>
2025-02-08 11:24:37 +1000

    задача на декоратор @property

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
-class Person:
-    def __init__(self, name, age):
-        self._name = name
-        self._age = age
+class Money:
+    def __init__(self, dollars, cents):
+        self.total_cents = dollars * 100 + cents  # Сохраняем общее количество центов
 
     @property
-    def name(self):
-        return self._name
+    def dollars(self):
+        return self.total_cents // 100  # Возвращаем количество долларов
+
+    @dollars.setter
+    def dollars(self, value):
+        if isinstance(value, int) and value >= 0:  # Проверяем, что значение корректно
+            self.total_cents = value * 100 + self.cents  # Обновляем total_cents, сохраняя центы
+        else:
+            print("Error dollars")  # Выводим сообщение об ошибке
 
     @property
-    def age(self):
-        return self._age
+    def cents(self):
+        return self.total_cents % 100  # Возвращаем количество центов
 
+    @cents.setter
+    def cents(self, value):
+        if isinstance(value, int) and 0 <= value < 100:  # Проверяем, что значение корректно
+            self.total_cents = self.dollars * 100 + value  # Обновляем total_cents, сохраняя доллары
+        else:
+            print("Error cents")  # Выводим сообщение об ошибке
 
-s = Person('Petr', 40)
-# s.age = 48
-
-print(s.age)
-print(s.name)
+    def __str__(self):
+        return f"Ваше состояние составляет {self.dollars} долларов {self.cents} центов"
