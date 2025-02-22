06cf97e6b751882187f6373ad22d4c938cb481e3
petrtimalev <petr.timalev@gmail.com>
2025-02-22 10:58:38 +1000

    update

--- main.py
+++ main.py
@@ -1,36 +1,17 @@
-class Ingredient:
-    def __init__(self, name, weight, price):
-        self.name = name
-        self.weight = weight
-        self.price = price  # стоимость за 100гр
+class MyClass:
+    class_attribute = "I am a class attribute"
 
-    @property
-    def cost(self):
-        n = (self.weight / 100) * self.price
-        return n
+    def __init__(self):
+        self.instance_attribute = "I am an instance attribute"
 
 
-class Pizza:
-    def __init__(self, name, ingredients=None):
-        self.name = name
-        # в пицца передается объект <__main__.Ingredient object at 0x10ad3bcb0> из ингредиентов
-        if ingredients is None:
-            self.ingredients = []
-        else:
-            self.ingredients = ingredients
+example_1 = MyClass()
+example_2 = MyClass()
+example_3 = MyClass()
 
-    @property
-    def cost(self):
+example_1.class_attribute = "Class attribute modified"
 
-        total_cost = sum(ingredient.cost for ingredient in self.ingredients)  # Сумма стоимости ингредиентов
-        return total_cost + 100  # Добавляем 100 рублей за основу
+print(example_1.class_attribute)
+print(example_3.class_attribute)
 
-
-chicken = Ingredient('chicken', 200, 80)
-mozzarella = Ingredient('mozzarella', 300, 110)
-sauce_bbq = Ingredient('sauce bbq', 150, 70)
-red_onion = Ingredient('red onion', 150, 50)
-barbecue = Pizza('BBQ', [chicken, mozzarella, sauce_bbq, red_onion])
-
-print('Стоимость пиццы BBQ', barbecue.cost)
-print(chicken)
+print(MyClass.class_attribute)
